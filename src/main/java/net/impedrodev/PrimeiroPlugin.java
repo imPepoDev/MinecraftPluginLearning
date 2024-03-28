cf8163e83c6fda07348eab3eb7272345337338cd
pedrokkj13 <pedrogabrielofc@hotmail.com>
2024-03-28 20:36:08 -0300

    first commit

---
+++ src/main/java/net/impedrodev/PrimeiroPlugin.java
@@ -0,0 +1,18 @@
+package net.impedrodev;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrimeiroPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
